9d29c05f3105fdfc18acb0a0d17c6c443f90edd7
Ian Dunn <ian@iandunn.name>
2022-11-09 12:15:19 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# WPorg Two-Factor
+
+WordPress.org-specific customizations for the Two Factor plugin
+
+## Security
+
+Please privately report any potential security issues to the [WordPress HackerOne](https://hackerone.com/wordpress) program.
---
+++ wporg-two-factor.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: WordPress.org Two Factor
+ * Description: WordPress.org-specific customizations for the Two Factor plugin.
+ * License:     GPLv2 or later
+ * Text Domain: wporg
+ * Network:     true
+ * Update URI:  false
+ */
+
+namespace WordPressdotorg\Two_Factor;
+defined( 'WPINC' ) || die();
+
