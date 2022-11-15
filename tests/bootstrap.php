9bbbefef76e59ae8d0877e8a3366dca4b9b42b23
Ian Dunn <ian@iandunn.name>
2022-11-14 17:19:06 -0800

    Tests: Setup phpunit

---
+++ tests/bootstrap.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped -- This is a shell script.
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php";
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+	require dirname( __DIR__ ) . '/wporg-two-factor.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
---
+++ tests/test-wporg-two-factor.php
@@ -0,0 +1,8 @@
+<?php
+
+use function WordPressdotorg\Two_Factor\{};
+
+defined( 'WPINC' ) || die();
+
+class Test_WPorg_Two_Factor extends WP_UnitTestCase {
+}
