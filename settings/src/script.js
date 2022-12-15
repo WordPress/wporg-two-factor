fc5d465601b0b95382b439ec92c01ff58b828223
Ian Dunn <ian@iandunn.name>
2022-12-15 13:04:12 -0800

    Rough in design for the Status screen
    See #31

--- settings/src/script.js
+++ settings/src/script.js
@@ -3,6 +3,8 @@
  */
 import { __ } from '@wordpress/i18n';
 import { render, useState } from '@wordpress/element';
+import { Card, CardBody, CardHeader } from '@wordpress/components';
+import { Icon, arrowLeft, cancelCircleFilled, check, chevronRight, warning } from '@wordpress/icons';
 
 window.addEventListener( 'DOMContentLoaded', renderSettings );
 
@@ -59,7 +61,8 @@ function Main() {
 			{ 'account-status' !== screen &&
 				<div className="wporg-2fa__navigation">
 					<a href="?screen=account-status" onClick={ ( event ) => clickScreenLink( event, 'account-status' ) }>
-						← Back
+						<Icon icon={ arrowLeft } />
+						Back
 					</a>
 				</div>
 			}
@@ -75,36 +78,101 @@ function Main() {
  * Render the Account Status.
  */
 function AccountStatus( { clickScreenLink } ) {
+	const emailStatus = 'pending';
+	const totpStatus = 'disabled';
+	// make dynamic
+
 	return (
 		<>
-			<p>
-				<a href="?screen=password" onClick={ ( event ) => clickScreenLink( event, 'password' ) }>
-					Password
-					You have a password configured, but can change it at any time.
-				</a>
-			</p>
+			<SettingStatusCard
+				screen="password"
+				status="enabled"
+				headerText="Password"
+				bodyText="You have a password configured, but can change it at any time"
+				clickScreenLink={ clickScreenLink }
+			/>
 
-			<p>
-				<a href="?screen=email" onClick={ ( event ) => clickScreenLink( event, 'email' ) }>
-					Account Email
-					Your account email address is foo@bar.com.
-				</a>
-			</p>
+			<SettingStatusCard
+				screen="email"
+				status={ emailStatus }
+				headerText="Account Email"
+				bodyText="Your account email address is foo@bar.com."
+				clickScreenLink={ clickScreenLink }
+			/>
 
-			<p>
-				<a href="?screen=two-factor-status" onClick={ ( event ) => clickScreenLink( event, 'two-factor-status' ) }>
-					Two Factor Authentication
-					You have two-factor authentication enabled using an app.
-				</a>
-			</p>
+			<SettingStatusCard
+				screen="two-factor-status"
+				status={ totpStatus }
+				headerText="Two Factor Authentication"
+				bodyText="You have two-factor authentication enabled using an app."
+				clickScreenLink={ clickScreenLink }
+			/>
 
-			<p>
-				<a href="?screen=backup-codes" onClick={ ( event ) => clickScreenLink( event, 'backup-codes' ) }>
-					Two-Factor Backup Codes
-					You have verified your backup codes for two-factor authentication.
-				</a>
-			</p>
+			<SettingStatusCard
+				screen="backup-codes"
+				status={ emailStatus }
+				headerText="Two-Factor Backup Codes"
+				bodyText="You have verified your backup codes for two-factor authentication."
+				clickScreenLink={ clickScreenLink }
+			/>
 		</>
+	);
+}
+
+/**
+ * Render a card for the status of the given setting.
+ */
+function SettingStatusCard( { screen, status, headerText, bodyText, clickScreenLink } ) {
+	// maybe use context for clickScreenLink instead of drilling
+
+	let screenUrl = new URL( document.location.href );
+	screenUrl.searchParams.set( 'screen', screen );
+
+	return (
+		<Card className={ 'wporg-2fa__status-card wporg-2fa__status-card-' + screen }>
+			<a
+				href={ screenUrl.href }
+				onClick={ ( event ) => clickScreenLink( event, screen ) }
+			>
+				<CardHeader>
+					<StatusIcon status={ status } />
+					{ headerText }
+				</CardHeader>
+
+				<CardBody>
+					<p>{ bodyText }</p>
+					<Icon icon={ chevronRight } />
+				</CardBody>
+			</a>
+		</Card>
+	);
+}
+
+/**
+ * Render the icon for the given status
+ */
+function StatusIcon( { status } ) {
+	let icon;
+
+	switch ( status ) {
+		case 'enabled':
+			icon = check;
+			break;
+
+		case 'pending':
+			icon = warning;
+			break;
+
+		case 'disabled':
+		default:
+			icon = cancelCircleFilled;
+	}
+
+	return (
+		<Icon
+			icon={ icon }
+			className={ 'wporg-2fa__status-icon is-' + status }
+		/>
 	);
 }
 
