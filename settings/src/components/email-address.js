b4588cf17e3bd21859a84086a8ddfda90279bbc8
Dion Hulse <dion@wordpress.org>
2022-12-21 09:17:38 +0000

    Perform the POST to update the user email.

--- settings/src/components/email-address.js
+++ settings/src/components/email-address.js
@@ -4,6 +4,8 @@
  */
 import { Button, TextControl } from '@wordpress/components';
 import { useState } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+import { saveUser, saveEntityRecord } from '@wordpress/core-data';
 
 /**
  * Render the Email setting.
@@ -14,6 +16,49 @@ export default function EmailAddress( { userData } ) {
 	// TODO: This is only needed until the parent component waits for the userData to load before rendering.
 	if ( null === email && userData && userData.email ) {
 		setEmail( userData.email );
+	}
+
+	/**
+	 * Update the email.
+	 */
+	function updateEmail( event ) {
+		// TODO: This doesn't cause the button to go back to a disabled state afterwards.
+
+		// send rest api request
+		// on success, update state to show that it's been saved
+
+		wp.data.dispatch('core').saveEntityRecord( 'root', 'user', { id: userData.id, email: email }, { throwOnError: true /* required for reject */ } ).then(
+			( response ) => {
+				console.log( "Success ", response );
+				userData = response;
+				alert( "Email has been updated" );
+			},
+			( error ) => {
+				console.log( "Error", error );
+				alert( error.message );
+			}
+		);
+/*
+		// This is the same as above, except that upon failure it never rejects, as throwOnError isn't settable.
+		wp.data.dispatch( 'core' ).saveUser( { id: userData.id, email: email } ).then(
+			( response ) => {
+				if ( ! response ) {
+					// Error occured during the save... Response data unavailable.
+					return;
+				}
+
+				console.log( "Success ", response );
+
+				userData = response;
+				alert( "Email has been updated" );
+			},
+			( error ) => {
+				// Never called, the promise is always resolved as successful.
+				console.log( "Error", error );
+				alert( error.message );
+			}
+		);
+*/
 	}
 
 	return (
@@ -39,13 +84,4 @@ export default function EmailAddress( { userData } ) {
 			</p>
 		</>
 	);
-}
-
-/**
- * Update the email.
- */
-function updateEmail( event ) {
-	console.log('save');
-	// send rest api request
-	// on success, update state to show that it's been saved
 }
