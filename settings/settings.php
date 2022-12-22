44d7e27f91bb7f0b47b9c9d5a1c5c73a55a08e14
Dion Hulse <dion@wordpress.org>
2022-12-22 02:56:51 +0000

    REST API: Add pre-loading of the users endpoint.

--- settings/settings.php
+++ settings/settings.php
@@ -39,5 +39,66 @@ function render_custom_ui() : void {
 	$user_id    = (int) bbp_get_displayed_user_id();
 	$json_attrs = json_encode( (object) [ 'userId' => $user_id ] );
 
+	$preload_paths = [
+		'/wp/v2/users/' . $user_id . '?context=edit',
+	];
+	preload_api_requests( $preload_paths );
+
 	echo do_blocks( "<!-- wp:wporg-two-factor/settings $json_attrs /-->" );
+}
+
+// reimplement block_editor_rest_api_preload() because of course there isn't a generic approach.
+/**
+ * Register/Output some REST-API calls to be pre-loaded.
+ *
+ * This prevents the browser having to make the HTTP call before the react UI can be ready.
+ * WARNING: This will output Javascript immediately if called during the page load if the wp-api-fetch script has already been output.
+ *
+ * @param array $preload_paths The REST API paths to be pre-loaded, must include prefixed slash.
+ * @return void
+ */
+function preload_api_requests( array $preload_paths ) : void {
+	global $post, $wp_scripts, $wp_styles;
+
+	/*
+	 * Ensure the global $post, $wp_scripts, and $wp_styles remain the same after
+	 * API data is preloaded.
+	 * Because API preloading can call the_content and other filters, plugins
+	 * can unexpectedly modify the global $post or enqueue assets which are not
+	 * intended for the block editor.
+	 */
+	$backup_global_post = ! empty( $post ) ? clone $post : $post;
+	$backup_wp_scripts  = ! empty( $wp_scripts ) ? clone $wp_scripts : $wp_scripts;
+	$backup_wp_styles   = ! empty( $wp_styles ) ? clone $wp_styles : $wp_styles;
+
+	$preload_data = array_reduce(
+		$preload_paths,
+		'rest_preload_api_request',
+		array()
+	);
+
+	// Restore the global $post, $wp_scripts, and $wp_styles as they were before API preloading.
+	$post       = $backup_global_post;
+	$wp_scripts = $backup_wp_scripts;
+	$wp_styles  = $backup_wp_styles;
+
+	$preload_js = sprintf(
+		'wp.apiFetch.use( wp.apiFetch.createPreloadingMiddleware( %s ) );',
+		wp_json_encode( $preload_data )
+	);
+
+	// The script may have already been printed. In that case, add the pre-loading inline now.
+	if ( wp_script_is( 'wp-api-fetch', 'done' ) ) {
+		printf(
+			'<script>%s</script>',
+			$preload_js
+		);
+	} else {
+		wp_enqueue_script( 'wp-api-fetch' );
+		wp_add_inline_script(
+			'wp-api-fetch',
+			$preload_js,
+			'after'
+		);
+	}
 }
