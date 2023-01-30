2e3cca5685aadf557501de20386e31b5c34f5ecc
Ian Dunn <ian@iandunn.name>
2023-01-30 11:16:38 -0800

    Progress Bar: Style to match mockup.
    Fixes #44

--- settings/src/components/setup-progress-bar.js
+++ settings/src/components/setup-progress-bar.js
@@ -5,16 +5,31 @@ import { Icon, lock, reusableBlock } from '@wordpress/icons';
 
 export default function SetupProgressBar( { step } ) {
 	return (
-		<ul className="wporg-2fa__setup-steps">
-			<li className={ 'totp' === step ? 'is-enabled' : 'is-disabled' }>
-				<Icon icon={ lock } />
-				Verify Codes
-			</li>
+		<div className="wporg-2fa__progress-bar">
+			<ul className="wporg-2fa__setup-steps">
+				<li className={ 'totp' === step ? 'is-enabled' : 'is-disabled' }>
+					<Icon icon={ lock } />
+					<br />
+					Verify Codes
+				</li>
 
-			<li className={ 'backup-codes' === step ? 'is-enabled' : 'is-disabled' }>
-				<Icon icon={ reusableBlock } />
-				Backup Codes
-			</li>
-		</ul>
+				<li className={ 'backup-codes' === step ? 'is-enabled' : 'is-disabled' }>
+					<Icon icon={ reusableBlock } />
+					<br />
+					Backup Codes
+				</li>
+			</ul>
+
+			<ul className="wporg-2fa__setup-step-separators">
+				<li className="wporg-2fa__step-separator is-enabled" />
+
+				<li className={
+					'wporg-2fa__step-separator ' +
+					( 'backup-codes' === step ? 'is-enabled' : 'is-disabled' )
+				} />
+
+				<li className="wporg-2fa__step-separator is-disabled" />
+			</ul>
+		</div>
 	);
 }
