0854d977ffaa287c6e64ec2d402ff8c8138c5bea
Ren <18050944+renintw@users.noreply.github.com>
2023-03-21 03:16:51 +0800

    Fix notice UI for 2fa, password, and backup codes (#82)

--- settings/src/components/totp.js
+++ settings/src/components/totp.js
@@ -213,7 +213,7 @@ function SetupForm( { handleEnable, verifyCode, setVerifyCode, qrCodeUrl, secret
 				required={ true }
 			/>
 
-			<div>
+			<div class="wporg-2fa__submit-btn-wrapper">
 				<Button type="submit" isPrimary disabled={ ! canSubmit }>
 					Enable
 				</Button>
