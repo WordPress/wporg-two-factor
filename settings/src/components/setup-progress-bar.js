a778b2d5a5a46ce5b370f762abf1ca96935d5f99
ren <18050944+renintw@users.noreply.github.com>
2023-03-28 19:59:35 +0800

    Add totp intro

--- settings/src/components/setup-progress-bar.js
+++ settings/src/components/setup-progress-bar.js
@@ -7,7 +7,13 @@ export default function SetupProgressBar( { step } ) {
 	return (
 		<div className="wporg-2fa__progress-bar">
 			<ul className="wporg-2fa__setup-steps">
-				<li className={ 'totp' === step ? 'is-enabled' : 'is-disabled' }>
+				<li className={ 'totp-intro' === step ? 'is-enabled' : 'is-disabled' }>
+					<Icon icon={ lock } />
+					<br />
+					Introduction
+				</li>
+
+				<li className={ 'totp-verify' === step ? 'is-enabled' : 'is-disabled' }>
 					<Icon icon={ lock } />
 					<br />
 					Verify Codes
