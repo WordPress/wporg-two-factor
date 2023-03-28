a8c8df487116bdecce70daa9b2b07275513f9c49
ren <18050944+renintw@users.noreply.github.com>
2023-03-28 23:17:45 +0800

    Component: Add AutoTabbingInput

--- settings/src/components/numeric-control.js
+++ settings/src/components/numeric-control.js
@@ -1,4 +1,9 @@
 /**
+ * WordPress dependencies
+ */
+import { useRef, useCallback } from '@wordpress/element';
+
+/**
  * Input field for values that use digits, but aren't strictly numbers in the mathematical sense.
  *
  * The classic example is a credit card, but in our context we have TOTP codes, backup codes, etc. We may want to
@@ -12,18 +17,32 @@
  * @link https://stackoverflow.com/a/66759105/450127
  */
 export default function NumericControl( props ) {
+	const { autoComplete, pattern, title, onChange, onKeyDown, index } = props;
+
+	const inputRef = useRef(null);
+
+	const handleChange = useCallback(
+		// Most callers will only need the value, so make it convenient for them.
+		event => onChange && onChange( event.target.value, event, index, inputRef ), []
+	)
+
+	const handleKeyDown = useCallback(
+		// Most callers will only need the value, so make it convenient for them.
+		event => onKeyDown && onKeyDown( event.target.value, event, index, inputRef ), []
+	)
+
 	return (
 		<input
 			{ ...props }
 			type="text"
 			inputMode="numeric"
-			autoComplete={ props.autoComplete || 'off' }
-			pattern={ props.pattern || "[0-9 ]*" }
-			title={ props.title || "Only numbers and spaces are allowed" }
-			onChange={ ( event ) => {
-				// Most callers will only need the value, so make it convenient for them.
-				props.onChange( event.target.value, event );
-			} }
+			ref={inputRef}
+			autoComplete={ autoComplete || 'off' }
+			pattern={ pattern || "[0-9 ]*" }
+			title={ title || "Only numbers and spaces are allowed" }
+			onChange={ handleChange }
+			onKeyDown={ handleKeyDown }
+			data-1p-ignore // Prevent 1Password from showing up
 		/>
 	);
 }
