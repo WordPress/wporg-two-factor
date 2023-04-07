17156fc7d4597c60dc7ada61087cca6ac35317a7
Ren <18050944+renintw@users.noreply.github.com>
2023-04-07 10:41:07 +0800

    Fix "show password" icon styles (#84)
    * Fix the toggle pushing issue.
    
    * Make the "Show password" icon inside input.
    
    * Add a toggled state for the show password icon
    
    * Avoid using inline functions for event handlers
    
    * Run lint:js and fix it.
    
    * Lint:css and fix it.
    
    * Adjust button focus style to match input field's
    
    * Rename handler function
    
    * Fix typo

--- settings/src/components/password.js
+++ settings/src/components/password.js
@@ -3,7 +3,7 @@
  *
  * window.zxcvbn is also used, but can't be enqueued via an `import` because the handle doesn't have a 'wp-'
  * prefix. Instead, it's enqueued via `block.json`. It also can't be declared/destructured here because it's
- * loaded asyncronously.
+ * loaded asynchronously.
  */
 import { pick } from 'lodash';
 import { generatePassword } from '@automattic/generate-password';
@@ -11,10 +11,21 @@ import { generatePassword } from '@automattic/generate-password';
 /**
  * WordPress dependencies
  */
-import { Button, Flex, Notice, TextControl }            from '@wordpress/components';
-import { useCallback, useContext, useEffect, useState } from '@wordpress/element';
-import { Icon, cancelCircleFilled, check, seen }        from '@wordpress/icons';
-import apiFetch                                         from '@wordpress/api-fetch';
+import { Button, Flex, Notice, TextControl } from '@wordpress/components';
+import {
+	useCallback,
+	useContext,
+	useEffect,
+	useState,
+} from '@wordpress/element';
+import {
+	Icon,
+	cancelCircleFilled,
+	check,
+	seen,
+	unseen,
+} from '@wordpress/icons';
+import apiFetch from '@wordpress/api-fetch';
 
 /**
  * Internal dependencies
@@ -26,11 +37,14 @@ import { GlobalContext } from '../script';
  */
 export default function Password() {
 	const { setGlobalNotice, userRecord } = useContext( GlobalContext );
-	const [ inputType, setInputType ]     = useState( 'password' );
-	let passwordStrong                    = true; // Saved passwords have already passed the test.
+	const [ inputType, setInputType ] = useState( 'password' );
+	let passwordStrong = true; // Saved passwords have already passed the test.
 
 	if ( userRecord.hasEdits ) {
-		passwordStrong = isPasswordStrong( userRecord.editedRecord.password, userRecord.record );
+		passwordStrong = isPasswordStrong(
+			userRecord.editedRecord.password,
+			userRecord.record
+		);
 	}
 
 	// Clear the "saved password" notice when password is being changed.
@@ -45,42 +59,60 @@ export default function Password() {
 	/**
 	 * Handle clicking the `Generate Password` button.
 	 */
-	const generatePasswordHandler = useCallback( async () => {
+	const handlePasswordGenerate = useCallback( async () => {
 		userRecord.edit( { password: generatePassword( 24, true, true ) } );
 		setInputType( 'text' );
 	}, [] );
 
 	// Handle form submission.
-	const formSubmitHandler = useCallback( async ( event ) => {
-		event.preventDefault();
+	const handleFormSubmit = useCallback(
+		async ( event ) => {
+			event.preventDefault();
 
-		if ( ! passwordStrong || userRecord.isSaving ) {
-			return;
-		}
+			if ( ! passwordStrong || userRecord.isSaving ) {
+				return;
+			}
 
-		await userRecord.save();
+			await userRecord.save();
 
-		// Changing the password resets the nonce, which causes subsequent API requests to fail. `apiFetch()` will
-		// retry them automatically, but that results in an extra XHR request and a console error.
-		const response = await apiFetch( {
-			url:   apiFetch.nonceEndpoint,
-			parse: false
-		} );
-		apiFetch.nonceMiddleware.nonce = await response.text();
+			// Changing the password resets the nonce, which causes subsequent API requests to fail. `apiFetch()` will
+			// retry them automatically, but that results in an extra XHR request and a console error.
+			const response = await apiFetch( {
+				url: apiFetch.nonceEndpoint,
+				parse: false,
+			} );
+			apiFetch.nonceMiddleware.nonce = await response.text();
 
-		setGlobalNotice( 'New password saved.' );
-	}, [ passwordStrong, userRecord.isSaving ] );
+			setGlobalNotice( 'New password saved.' );
+		},
+		[ passwordStrong, userRecord.isSaving ]
+	);
+
+	const handlePasswordChange = useCallback(
+		( password ) => userRecord.edit( { password } ),
+		[]
+	);
+
+	const handlePasswordToggle = useCallback(
+		() => setInputType( inputType === 'password' ? 'text' : 'password' ),
+		[ inputType ]
+	);
 
 	return (
-		<form onSubmit={ formSubmitHandler }>
+		<form onSubmit={ handleFormSubmit }>
 			<p>
-				To update your password enter a new one below.
-				Strong passwords are random, at least twenty characters long, and include uppercase letters and symbols.
+				To update your password enter a new one below. Strong passwords
+				are random, at least twenty characters long, and include
+				uppercase letters and symbols.
 			</p>
 
 			<p>
-				For convenience, use a password manager to store and automatically enter passwords.
-				For more information, read about <a href="https://wordpress.org/documentation/article/password-best-practices/">password best practices</a>.
+				For convenience, use a password manager to store and
+				automatically enter passwords. For more information, read about{ ' ' }
+				<a href="https://wordpress.org/documentation/article/password-best-practices/">
+					password best practices
+				</a>
+				.
 			</p>
 
 			<Flex className="wporg-2fa__password_container">
@@ -94,50 +126,60 @@ export default function Password() {
 					size="62"
 					value={ userRecord.editedRecord.password ?? '' }
 					placeholder="Q1jtBPRmROv51KOtbZ5aIKrc"
-					onChange={ ( password ) => userRecord.edit( { password } ) }
+					onChange={ handlePasswordChange }
 				/>
-
 				<Button
 					className="wporg-2fa__show-password"
 					size={ 24 }
-					onClick={ () => setInputType( inputType === 'password' ? 'text' : 'password' ) }
-					aria-label="Show password"
+					onClick={ handlePasswordToggle }
+					aria-label={
+						inputType === 'password'
+							? 'Show Password'
+							: 'Hide Password'
+					}
+					title={
+						inputType === 'password'
+							? 'Show Password'
+							: 'Hide Password'
+					}
 				>
-					<Icon icon={ seen } />
+					<Icon icon={ inputType === 'password' ? seen : unseen } />
 				</Button>
 			</Flex>
 
-			{ userRecord.hasEdits && passwordStrong &&
+			{ userRecord.hasEdits && passwordStrong && (
 				<Notice status="success" isDismissible={ false }>
 					<Icon icon={ check } />
 					Your password is strong enough to be saved.
 				</Notice>
-			}
+			) }
 
-			{ userRecord.hasEdits && ! passwordStrong &&
+			{ userRecord.hasEdits && ! passwordStrong && (
 				<Notice status="error" isDismissible={ false }>
 					<Icon icon={ cancelCircleFilled } />
-					That password is too easy to compromise. Please make it longer and/or add random numbers/symbols.
+					That password is too easy to compromise. Please make it
+					longer and/or add random numbers/symbols.
 				</Notice>
-			}
+			) }
 
 			<p>
 				<Button
 					isPrimary
-					disabled={ passwordStrong && ! userRecord.isSaving ? '' : 'disabled' }
+					disabled={
+						passwordStrong && ! userRecord.isSaving
+							? ''
+							: 'disabled'
+					}
 					type="submit"
 				>
 					{ userRecord.isSaving ? 'Saving...' : 'Save password' }
 				</Button>
 
-				{ window.crypto?.getRandomValues &&
-					<Button
-						isSecondary
-						onClick={ generatePasswordHandler }
-					>
+				{ window.crypto?.getRandomValues && (
+					<Button isSecondary onClick={ handlePasswordGenerate }>
 						Generate strong password
 					</Button>
-				}
+				) }
 			</p>
 		</form>
 	);
@@ -148,25 +190,35 @@ export default function Password() {
  *
  * Validation is also done on the backend by `security-weak-passwords.php`.
  *
- * @returns {boolean}
+ * @param {string} password
+ * @param {Object} userData
+ * @return {boolean} true if the password is strong, false otherwise
  */
 function isPasswordStrong( password, userData ) {
-	const { zxcvbn } = window; // Done here because it's loaded asyncronously.
+	const { zxcvbn } = window; // Done here because it's loaded asynchronously.
 
 	if ( ! zxcvbn ) {
 		return false; // Not loaded yet.
 	}
 
-	let blocklist = Object.values( pick(
-		userData,
-		[ 'email', 'description', 'first_name', 'last_name', 'name', 'nickname', 'slug', 'username' ]
-	) );
+	let blocklist = Object.values(
+		pick( userData, [
+			'email',
+			'description',
+			'first_name',
+			'last_name',
+			'name',
+			'nickname',
+			'slug',
+			'username',
+		] )
+	);
 	blocklist = blocklist.concat( [ 'wordpress', 'wporg', 'wordpressorg' ] );
 
 	// `3` is "safely unguessable: moderate protection from offline slow-hash scenario. (guesses < 10^10)"
 	// `4` is "very unguessable: strong protection from offline slow-hash scenario. (guesses >= 10^10)"
-	const minimumScore = userData['2fa_required'] ? 4 : 3;
-	const strength     = zxcvbn( password, blocklist );
+	const minimumScore = userData[ '2fa_required' ] ? 4 : 3;
+	const strength = zxcvbn( password, blocklist );
 
 	return strength.score >= minimumScore;
 }
