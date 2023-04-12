b30329616fe09b346b61cbb9576d0f1d48d168d3
Dion Hulse <dion@wordpress.org>
2023-04-12 07:31:58 +0000

    Upgrade unencrypted keys to encrypted upon retrieval.

--- class-encrypted-totp-provider.php
+++ class-encrypted-totp-provider.php
@@ -25,23 +25,27 @@ class Encrypted_Totp_Provider extends Two_Factor_Totp {
 			$key = wporg_encrypt( $key, 'two-factor' );
 		}
 
-		return parent::set_user_totp_key( $user_id, $key );
+		return parent::set_user_totp_key( $user_id, (string) $key );
 	}
 
 	/**
 	 * When retrieving the key, decrypt it first.
+	 *
+	 * If the key isn't currently stored encrypted, it's upgraded to encrypted status.
 	 *
 	 * @param int $user_id User ID.
 	 * @return string|false TOTP key, or false if not set.
 	 */
 	public function get_user_totp_key( $user_id ) {
 		$key = parent::get_user_totp_key( $user_id );
-		if ( ! $key ) {
-			return $key;
-		}
 
-		if ( function_exists( 'wporg_maybe_decrypt' ) ) {
-			$key = wporg_maybe_decrypt( $key, 'two-factor' );
+		if ( $key && function_exists( 'wporg_is_encrypted' ) ) {
+			if ( wporg_is_encrypted( $key ) ) {
+				$key = (string) wporg_decrypt( $key, 'two-factor' );
+			} else {
+				// Upgrade the key to be encrypted.
+				$this->set_user_totp_key( $user_id, $key );
+			}
 		}
 
 		return $key;
