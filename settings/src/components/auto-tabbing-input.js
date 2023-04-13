35d524f288a8af424503718d2c9e372426eb215d
Ren <18050944+renintw@users.noreply.github.com>
2023-04-13 17:48:54 +0800

    Split TOTP instruction into two steps (#100)
    * Add TOTP intro
    
    *  Set a fixed height for the screen
    
    * Component: Add AutoTabbingInput
    
    * Use AutoTabbingInput
    
    * Align TOTP styles w/ design
    
    * Fix QR-code height to avoid jitter
    
    * Make class name consistent
    
    * Add borders to inputs when there are errors
    
    * Let Cancel clear all the inputs

---
+++ settings/src/components/auto-tabbing-input.js
@@ -0,0 +1,67 @@
+/**
+ * WordPress dependencies
+ */
+import { useCallback } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import NumericControl    from './numeric-control';
+
+const AutoTabbingInput = ( props ) => {
+    const  { inputs, setInputs, onComplete, error } = props;
+
+    const handleChange = useCallback( (value, event, index, inputRef) => {
+
+        setInputs((prevInputs) => {
+            const newInputs = [...prevInputs];
+
+            // Clean input
+            if (value.trim() === "") {
+                event.target.value = "";
+                value  = "";
+            }
+            
+            newInputs[index] = value;
+
+            // Check if all inputs are filled
+            const allFilled = newInputs.every((input) => "" !== input);
+            if (allFilled && onComplete) {
+                onComplete(true);
+            } else {
+                onComplete(false);
+            }
+
+            return newInputs;
+        });
+
+        if (value && "" !== value.trim() && inputRef.current.nextElementSibling) {
+            inputRef.current.nextElementSibling.focus();
+        }
+    }, []);
+  
+    const handleKeyDown = useCallback( (value, event, index, inputRef) => {
+        if (event.key === 'Backspace' && ! value && inputRef.current.previousElementSibling) {
+            inputRef.current.previousElementSibling.focus();
+        }
+    }, []);
+
+    return (
+        <div className={"wporg-2fa__auto-tabbing-input" + ( error ? " is-error" : "" )}>
+          {inputs.map((value, index) => (
+            <NumericControl
+                {...props}
+                value={value}
+                key={index}
+                index={index}
+                onChange={handleChange}
+                onKeyDown={handleKeyDown}
+                maxLength="1"
+                required
+            />
+          ))}
+        </div>
+    );
+  };
+  
+  export default AutoTabbingInput;
--- settings/src/components/setup-progress-bar.js
+++ settings/src/components/setup-progress-bar.js
@@ -7,10 +7,16 @@ export default function SetupProgressBar( { step } ) {
 	return (
 		<div className="wporg-2fa__progress-bar">
 			<ul className="wporg-2fa__setup-steps">
-				<li className={ 'totp' === step ? 'is-enabled' : 'is-disabled' }>
+				<li className={ 'totp-intro' === step ? 'is-enabled' : 'is-disabled' }>
 					<Icon icon={ lock } />
 					<br />
-					Verify Codes
+					Introduction
+				</li>
+
+				<li className={ 'totp-setup' === step ? 'is-enabled' : 'is-disabled' }>
+					<Icon icon={ lock } />
+					<br />
+					Scan QR Code
 				</li>
 
 				<li className={ 'backup-codes' === step ? 'is-enabled' : 'is-disabled' }>
