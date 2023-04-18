e3037be12e8ea5dc8d689dcf85130cc81b0be9e1
ren <18050944+renintw@users.noreply.github.com>
2023-04-18 22:09:06 +0800

    Remove Intro Screen

--- settings/src/components/setup-progress-bar.js
+++ settings/src/components/setup-progress-bar.js
@@ -7,12 +7,6 @@ export default function SetupProgressBar( { step } ) {
 	return (
 		<div className="wporg-2fa__progress-bar">
 			<ul className="wporg-2fa__setup-steps">
-				<li className={ 'totp-intro' === step ? 'is-enabled' : 'is-disabled' }>
-					<Icon icon={ lock } />
-					<br />
-					Introduction
-				</li>
-
 				<li className={ 'totp-setup' === step ? 'is-enabled' : 'is-disabled' }>
 					<Icon icon={ lock } />
 					<br />
