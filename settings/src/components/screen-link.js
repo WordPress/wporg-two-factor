5d99f1c79933ad1e0153831fb97d5f3deef80de0
Ian Dunn <ian@iandunn.name>
2023-09-14 16:43:05 -0700

    Refresh user when navigating to Account Status

--- settings/src/components/screen-link.js
+++ settings/src/components/screen-link.js
@@ -1,17 +1,23 @@
 /**
  * WordPress dependencies
  */
-import { useCallback, useContext } from '@wordpress/element';
+import { useCallback, useContext, useState } from '@wordpress/element';
+import { Spinner } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { GlobalContext } from '../script';
+import { refreshRecord } from '../utilities/common';
 
 export default function ScreenLink( { screen, anchorText, buttonStyle = false, ariaLabel } ) {
-	const { navigateToScreen } = useContext( GlobalContext );
+	const {
+		user: { userRecord },
+		navigateToScreen,
+	} = useContext( GlobalContext );
 	const classes = [];
 	const screenUrl = new URL( document.location.href );
+	const [ refreshing, setRefreshing ] = useState( false );
 
 	screenUrl.searchParams.set( 'screen', screen );
 
@@ -24,11 +30,25 @@ export default function ScreenLink( { screen, anchorText, buttonStyle = false, a
 	}
 
 	const onClick = useCallback(
-		( event ) => {
+		async ( event ) => {
 			event.preventDefault();
+
+			if ( refreshing ) {
+				return;
+			}
+
+			setRefreshing( true );
+
+			// Sometimes the record will have been updated, and those changes need to be reflected on Account
+			// Status, but it's not possible for the current screen to refresh the record. For example, Backup
+			// Codes can't refresh it if the user generates codes but then clicks "Back" before clicking "All Finished".
+			if ( 'account-status' === screen ) {
+				await refreshRecord( userRecord );
+			}
+
 			navigateToScreen( screen );
 		},
-		[ navigateToScreen ]
+		[ navigateToScreen, userRecord, screen ]
 	);
 
 	return (
@@ -39,6 +59,8 @@ export default function ScreenLink( { screen, anchorText, buttonStyle = false, a
 			aria-label={ ariaLabel }
 		>
 			{ anchorText }
+
+			{ refreshing && <Spinner /> }
 		</a>
 	);
 }
