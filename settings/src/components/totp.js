5795176e796c968c26b7b77d7215265d9a618a42
StevenDufresne <dufresnesteven@gmail.com>
2023-09-15 12:16:25 +0900

    Add isDestructive to the totp disable

--- settings/src/components/totp.js
+++ settings/src/components/totp.js
@@ -454,7 +454,7 @@ function ConfirmDisableApp( { onConfirm, onClose, disabling } ) {
 				</div>
 			) : (
 				<div className="wporg-2fa__submit-actions">
-					<Button variant="primary" onClick={ onConfirm }>
+					<Button variant="primary" isDestructive onClick={ onConfirm }>
 						Disable
 					</Button>
 
