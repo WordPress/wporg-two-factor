c27d9618025c85cf4cc4e1fab47210bdbc77a9b3
Dion Hulse <dion@wordpress.org>
2024-07-08 05:15:40 +0000

    rename for consistency.

--- wporg-two-factor.php
+++ wporg-two-factor.php
@@ -30,7 +30,7 @@ function is_2fa_beta_tester( $user = false ) : bool {
 
 require_once __DIR__ . '/settings/settings.php';
 require_once __DIR__ . '/stats.php';
-require_once __DIR__ . '/revalidate-modal.php';
+require_once __DIR__ . '/revalidation.php';
 
 /**
  * Load the WebAuthn plugin.
