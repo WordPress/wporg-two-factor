a18ea76d63cb6f364dc07530c850de5dc02151d8
Dion Hulse <dion@wordpress.org>
2024-08-01 06:35:03 +0000

    Export the functions for use by other scripts, simplify code and null checks, merge the link handlers.

--- revalidation/script.js
+++ revalidation/script.js
@@ -1,24 +1,40 @@
-(function(settings){
-	let revalidateModal = false;
-	let lastTarget = false;
+window.wp = window.wp || {};
 
-	const revalidateRequired = function() {
+( function( settings, wp ) {
+	let revalidateModal = false;
+	let triggerEvent = false
+
+	// Returns the expiry time of the sudo cookie.
+	const getRevalidateExpiry = function() {
 		const sudoCookieValue = document.cookie.split( /;\s*/ ).filter(
 			(cookie) => cookie.startsWith( settings.cookieName + '=' )
 		)[0]?.split('=')[1] || false;
 
-		return ! sudoCookieValue;
+		if ( ! sudoCookieValue ) {
+			return false;
+		}
+
+		const expiry = new Date( parseInt( sudoCookieValue ) * 1000 );
+		if ( expiry < new Date() ) {
+			return false;
+		}
+
+		return expiry;
 	};
 
+	// Whether or not revalidation is required.
+	const revalidateRequired = function() {
+		return ! getRevalidateExpiry();
+	};
+
+	// Does the provided URL look like a revalidation url?
 	const urlLooksLikeRevalidationURL = function( url ) {
 		return url.includes( 'wp-login.php' ) && url.includes( 'action=revalidate_2fa' );
-	}
+	};
 
-	const modalTrigger = function(e) {
-		e.preventDefault();
-		lastTarget = e.target;
-
-		// Remove the existing dialog from the DOM.
+	// Display a modal dialog asking to revalidate.
+	const displayModal = function() {
+		// Remove any existing dialog from the DOM.
 		if ( revalidateModal ) {
 			revalidateModal.remove();
 		}
@@ -28,7 +44,7 @@
 		revalidateModal.innerHTML = '<h1>' + settings.l10n.title + '</h1>';
 
 		const iframe    = document.createElement( 'iframe' );
-		const linkHref  = e.currentTarget.href || e.target.href;
+		const linkHref  = triggerEvent?.currentTarget?.href || triggerEvent?.target?.href;
 		const iframeSrc = urlLooksLikeRevalidationURL( linkHref ) ? linkHref : settings.url;
 
 		iframe.src = iframeSrc + '&interim-login=1';
@@ -47,6 +63,7 @@
 		revalidateModal.showModal();
 	};
 
+	// Remove the revalidate URL from the link, replacing it with the redirect_to if present.
 	const maybeRemoveRevalidateURL = function( element ) {
 		// If we're on a element within a link, run back up the DOM to the proper parent.
 		while ( element && element.tagName !== 'A' && element.parentElement ) {
@@ -76,6 +93,7 @@
 		return true;
 	};
 
+	// Handle the click event on a link, checking if revalidation is required prior to proceeding.
 	const maybeRevalidateOnLinkNavigate = function( e ) {
 		// Check to see if revalidation is required, otherwise we're in Sudo mode.
 		if ( ! revalidateRequired() ) {
@@ -83,50 +101,59 @@
 			return;
 		}
 
-		// If we're here, we need to revalidate the session, trigger the modal.
-		modalTrigger( e );
+		triggerEvent = e;
 
-		// Wait for the revalidation complete event, and re-trigger this navigation.
-		e.target.addEventListener( 'reValidationComplete', function() {
-			e.target.dispatchEvent( e ); // Just throw the exact same event again.
-		} );
+		// Prevent the default action.
+		e.preventDefault();
+
+		// If we're here, we need to revalidate the session, trigger the modal.
+		displayModal();
 	};
 
-	// Attach event listeners to all revalidate links
-	document.querySelectorAll( 'a[href*="revalidate_2fa"]' ).forEach( function( el ) {
-		el.addEventListener( 'click', modalTrigger );
-	} );
-
-	// Attach event listeners to any links that require a valid 2FA session.
-	document.querySelectorAll( 'a[data-2fa-required]' ).forEach( function( el ) {
-		el.addEventListener( 'click', maybeRevalidateOnLinkNavigate );
-
-		// If we need to revalidate, attach a class to said elements.
-		if ( revalidateRequired() ) {
-			el.classList.add( 'revalidate-required' );
-		}
-	} );
-
-	// Listen for the revalidation to complete.
-	window.addEventListener( 'message', function( event ) {
-		if ( ! event.data || ! event.data.type || event.data.type !== 'reValidationComplete' ) {
+	// Wait for the revalidation to complete.
+	const messageHandler = function( event ) {
+		if ( event?.data?.type !== 'reValidationComplete' ) {
 			return;
 		}
 
 		revalidateModal.close();
 		revalidateModal.remove();
 
-		// We've revalidated, remove any revalidate-required classes.
-		document.querySelectorAll( '.revalidate-required' ).forEach( function( el ) {
-			el.classList.remove( 'revalidate-required' );
-		} );
+		// Import and reset.
+		const theTriggerEvent = triggerEvent;
+		triggerEvent = false;
 
 		// Maybe remove the revalidate URL from the last target.
-		if ( lastTarget ) {
-			maybeRemoveRevalidateURL( lastTarget );
+		if ( theTriggerEvent?.target ) {
+			maybeRemoveRevalidateURL( theTriggerEvent.target );
 		}
 
 		// Finally, notify others.
-		( lastTarget || window ).dispatchEvent( new Event( 'reValidationComplete', { bubbles: true } ) );
-	} );
-})(wporgTwoFactorRevalidation);
+		( theTriggerEvent?.target || window ).dispatchEvent( new Event( 'reValidationComplete', { bubbles: true } ) );
+
+		// If the last event was a click, throw that again.
+		if ( theTriggerEvent?.type === 'click' ) {
+			theTriggerEvent.target.dispatchEvent( theTriggerEvent );
+		}
+	};
+
+	// Export these functions for other scripts and debugging.
+	wp.wporg2faRevalidation = {
+		getRevalidateExpiry,
+		revalidateRequired,
+		urlLooksLikeRevalidationURL,
+		displayModal,
+		maybeRemoveRevalidateURL,
+		maybeRevalidateOnLinkNavigate,
+		messageHandler,
+	};
+
+	// Attach event listeners to all revalidate links and those that require 2FA sessions.
+	document.querySelectorAll( 'a[href*="action=revalidate_2fa"], a[data-2fa-required]' ).forEach(
+		(el) => el.addEventListener( 'click', maybeRevalidateOnLinkNavigate )
+	);
+
+	// Watch for revalidation completion.
+	window.addEventListener( 'message', messageHandler );
+
+} )( wporgTwoFactorRevalidation, window.wp );
