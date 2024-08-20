f8482cd87c1c5d0fc70bf960553ef95a476f2af4
Dion Hulse <dion@wordpress.org>
2024-08-20 12:52:56 +1000

    Linting was unhappy with linting suggestion.

--- settings/src/components/backup-codes.js
+++ settings/src/components/backup-codes.js
@@ -233,8 +233,8 @@ function Manage( { setRegenerating } ) {
 						<div>
 							You only have <strong>{ backupCodesRemaining }</strong> backup codes
 							remaining. Please regenerate and save new ones before you run out. If
-							you don&apos;t, you won&apos;t be able to log into your account if
-							you lose your phone.
+							you don&apos;t, you won&apos;t be able to log into your account if you
+							lose your phone.
 						</div>
 					</Notice>
 				) }
