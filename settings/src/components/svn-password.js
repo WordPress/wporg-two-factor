4f358fc71bd9f76cee2559230a2c38ed613ec3e7
Dion Hulse <dion@wordpress.org>
2024-08-23 05:47:50 +0000

    Updated interface design

--- settings/src/components/svn-password.js
+++ settings/src/components/svn-password.js
@@ -2,9 +2,8 @@
  * WordPress dependencies
  */
 import apiFetch from '@wordpress/api-fetch';
-import { Button, Notice, Spinner } from '@wordpress/components';
+import { Button } from '@wordpress/components';
 import { useCallback, useContext, useState } from '@wordpress/element';
-import { Icon, check } from '@wordpress/icons';
 import { refreshRecord } from '../utilities/common';
 import CopyToClipboardButton from './copy-to-clipboard-button';
 
@@ -60,43 +59,39 @@ export default function SVNPassword() {
 			</p>
 
 			<p>
-				Your username for SVN is the same as your WordPress.org account login, but is
-				case-sensitive. When using SVN you will need to use the username{ ' ' }
-				<code>{ userRecord.record.username }</code>. For security, your WordPress.org
-				account password cannot be used to commit to SVN, you must use a separate SVN
-				password, which you can generate here.
+				For security, your WordPress.org account password should not be used to commit to
+				SVN, use a separate SVN password, which you can generate here.
 			</p>
 
+			<h3>Details</h3>
 			<p>
-				If you lose your SVN password, you can replace it by generating a new one at any
-				time. All previous SVN passwords will be invalidated immediately.
+				Username: <code>{ userRecord.record.username }</code>
+				<br />
+				Password:{ ' ' }
+				{ generatedPassword || userRecord.record.svn_password ? (
+					<>
+						<code>
+							{ generatedPassword || 'svn_****************************************' }
+						</code>
+						{ generatedPassword && (
+							<CopyToClipboardButton text={ generatedPassword } />
+						) }
+					</>
+				) : (
+					<>
+						<em>Not configured</em>
+					</>
+				) }
 			</p>
 
-			{ generatedPassword && (
-				<Notice
-					status="success"
-					isDismissible={ true }
-					onRemove={ () => setGeneratedPassword( false ) }
-				>
-					<Icon icon={ check } />
-					Your new SVN Password:{ ' ' }
-					<code>{ isGenerating ? <Spinner /> : generatedPassword }</code>
-					<CopyToClipboardButton codes={ generatedPassword } />
-				</Notice>
-			) }
-
-			<p className="wporg-2fa__submit-actions">
-				<Button variant="primary" onClick={ handleGenerate } disabled={ isGenerating }>
-					{ isGenerating ? (
-						<>
-							<Spinner />
-							Generating..
-						</>
-					) : (
-						<>Generate password</>
-					) }
-				</Button>
-			</p>
+			<Button
+				variant="secondary"
+				onClick={ handleGenerate }
+				isBusy={ isGenerating }
+				disabled={ isGenerating }
+			>
+				Regenerate Password
+			</Button>
 		</>
 	);
 }
