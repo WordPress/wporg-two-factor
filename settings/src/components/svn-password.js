ad8dfc105cac247614e04daa764640caa9dd9ef7
StevenDufresne <dufresnesteven@gmail.com>
2024-08-29 12:38:17 +0900

    Fix linter issue.

--- settings/src/components/svn-password.js
+++ settings/src/components/svn-password.js
@@ -91,7 +91,10 @@ export default function SVNPassword() {
 							<code>{ generatedPassword || 'svn_*****************' }</code>
 							&nbsp;
 							{ generatedPassword && (
-								<CopyToClipboardButton variant="link" contents={ generatedPassword } />
+								<CopyToClipboardButton
+									variant="link"
+									contents={ generatedPassword }
+								/>
 							) }
 							{ userRecord.record.svn_password_created && (
 								<div className="wporg-2fa__svn-password_generated">
